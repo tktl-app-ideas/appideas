d5f956479a824ff05ae03b2328ce9a2850e982cd
satukark <satu81@hotmail.com>
2014-01-16 13:15:56 +0100

    Create Idea table by Scaffolding
    Include name and description.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140116121251) do
+
+  create_table "ideas", force: true do |t|
+    t.string   "name"
+    t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
