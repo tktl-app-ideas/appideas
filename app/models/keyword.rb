6a0f2d77b81ac7ae205849ac6df6f2e50b9cf7fd
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2014-01-22 10:39:09 +0200

    Implement Idea-to-Keyword relation
    Add relation methods to the models.

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,5 @@
 class Keyword < ActiveRecord::Base
+
+		has_many :idea_keywords
+		has_many :ideas, through: :idea_keywords
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140121091544) do
+ActiveRecord::Schema.define(version: 20140121124943) do
+
+  create_table "idea_keywords", force: true do |t|
+    t.integer  "idea_id"
+    t.integer  "keyword_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ideas", force: true do |t|
     t.string   "name"
