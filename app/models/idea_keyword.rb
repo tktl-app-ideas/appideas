f9d62518483ad9e2080697e8d34e9c6dbba2a5d9
Aki Kesulahti <aki.kesulahti@gmail.com>
2014-01-28 13:16:14 +0200

    Fix indentation
    Fixed all .rb files to make correct indentation
    http://stackoverflow.com/questions/3218528/indenting-in-vim-with-all-the-files-in-folder

--- app/models/idea_keyword.rb
+++ app/models/idea_keyword.rb
@@ -1,4 +1,4 @@
 class IdeaKeyword < ActiveRecord::Base
-		belongs_to :idea
-		belongs_to :keyword
+  belongs_to :idea
+  belongs_to :keyword
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,4 @@ Appideas::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-end
+  end
