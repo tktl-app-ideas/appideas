780c5e60038df4a6e2930321bc5cfe47d72e4c19
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2014-01-29 12:37:56 +0200

    Add fabrication gem to the Project
    Fabricator also used in a test case of Idea model.

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe Idea do
+  let(:idea) { Fabricate(:idea) }
+
   describe "that has been created with a keyword" do
-    it "should provide a method to list all its keywords" do
-    end
   end
+
+  it "should provide a method to list all its keywords" do
+    idea.respond_to?(:keywords).should be true
+  end
+
   it "should have a method hello" do
-    idea = Idea.new
-    idea.respond_to?(:hello).should == true
+    idea.respond_to?(:hello).should be true
+  end
+
+  it "should have a description field" do
+    idea.desc.include?("Bacon").should be true
   end
 end
