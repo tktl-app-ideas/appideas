369cdbad4a9085b8e7e385fc909b48c63dc065e3
Heidi Jauhiainen <wikgren@gmail.com>
2014-01-30 11:30:27 +0200

    Add dropbox from where a Keyword can be attached to an Idea
    Keyword cannot be added if Idea already has it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-
-  
   @words = Keyword.all
 
 
