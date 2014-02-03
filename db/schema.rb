9c3997733c4fd9814a11d7cde76b187444cb2c64
Satu Kärkkäinen <iltasatu@gmail.com>
2014-02-03 19:33:42 +0100

    gem file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140121124943) do
+ActiveRecord::Schema.define(version: 20140203174154) do
 
   create_table "idea_keywords", force: true do |t|
     t.integer  "idea_id"
@@ -33,5 +33,20 @@ ActiveRecord::Schema.define(version: 20140121124943) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "votes", force: true do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
