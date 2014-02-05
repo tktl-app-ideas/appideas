c8ed365bc9c12f23ca9223715344813d3ffb061f
Satu Kärkkäinen <iltasatu@gmail.com>
2014-02-05 12:09:57 +0100

    Take 'Keywords' away from navbar & create admin/keywords route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Appideas::Application.routes.draw do
   resources :ideas
   resources :users
 
+  get 'admin/keywords', to: 'keywords#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
