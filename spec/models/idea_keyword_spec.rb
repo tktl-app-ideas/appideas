048ff9b97a9720da8f335af5415997c3885a3ef9
Heidi Jauhiainen <wikgren@gmail.com>
2014-02-06 16:28:02 +0200

    Add validation to existing models
    Also add tests to check the validations and correct existing tests. #74

--- spec/models/idea_keyword_spec.rb
+++ spec/models/idea_keyword_spec.rb
@@ -1,34 +1,37 @@
 require 'spec_helper'
 
 describe IdeaKeyword do
+
+  before(:each) do
+    @idea = Idea.create name:"test", desc:"Description"
+  end
   it "there is working link between one Idea and one Keyword" do
-    idea = Idea.create name:"test"
+    
     kw = Keyword.create name:"kwtest"
 
-    idea.keywords << kw
+    @idea.keywords << kw
 
-    expect(idea.keywords.count).to eq(1)
+    expect(@idea.keywords.count).to eq(1)
   end    
 
   it "you can add several Keywords to one Idea" do
-    idea = Idea.create name:"test"
+    
     kw1 = Keyword.create name:"kwtest1"
     kw2 = Keyword.create name:"kwtest2"
     kw3 = Keyword.create name:"kwtest3"
     
-    idea.keywords << kw1
-    idea.keywords << kw2
-    idea.keywords << kw3
+    @idea.keywords << kw1
+    @idea.keywords << kw2
+    @idea.keywords << kw3
 
-    expect(idea.keywords.count).to eq(3)
+    expect(@idea.keywords.count).to eq(3)
   end
 
   it "you can add one Keyword to several Ideas" do
-    idea1 = Idea.create name:"test"
-    idea2 = Idea.create name:"test2"
+    idea2 = Idea.create name:"test2", desc:"Description2"
     kw = Keyword.create name:"kwtest"
 
-    idea1.keywords << kw
+    @idea.keywords << kw
     idea2.keywords << kw
     
     expect(kw.ideas.count).to eq(2)
