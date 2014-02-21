9eb048ff2e3be990f93f0780cd9f8584bea8c8a1
Heidi Jauhiainen <wikgren@gmail.com>
2014-02-21 15:04:19 +0200

    Add listing of comments and adding a new comment to the Idea details view

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,6 +4,7 @@ class IdeasController < ApplicationController
 		
   before_action :set_idea, only: [:show, :edit, :update, :destroy]
   before_action :set_keywords
+  #before_action :set_comments
     
   # GET /ideas
   # GET /ideas.json
@@ -28,20 +29,22 @@ class IdeasController < ApplicationController
   # GET /ideas/1
 	# GET /ideas/1.json
   def show
-    idea = Idea.find(params[:id])
-    @keywords = idea.keywords
+    @idea = Idea.find(params[:id])
+    # get comments from db
+    @comments = Comment.find_comments_for_commentable( "Idea", @idea.id )
+    @keywords = @idea.keywords
     @words = @keywords
     @all = Keyword.all
     keyword_id = params[:keyword_id]
     remove_id = params[:remove_id]
     unless remove_id.nil? or remove_id.empty?
       #keyword = Keyword.find(remove_id)
-      idea.keywords.delete(remove_id)
+      @idea.keywords.delete(remove_id)
     end
     unless keyword_id.nil? or keyword_id.empty?
       keyword = Keyword.find(keyword_id)
-      idea.keywords << keyword
-      redirect_to idea_path(idea)
+      @idea.keywords << keyword
+      redirect_to idea_path(@idea)
     end
   end
     
@@ -49,7 +52,6 @@ class IdeasController < ApplicationController
   # GET /ideas/new
   def new
     @idea = Idea.new
-
     @keywords = Keyword.all
   end
 
@@ -58,11 +60,11 @@ class IdeasController < ApplicationController
     @keywords = Keyword.all
     @all = Keyword.all
   end
+
 # POST /ideas # POST /ideas.json
   def create
     @idea = Idea.new(idea_params)
     keyword_ids = params[:idea][:keyword_ids]
- 
     if @idea.save
       keywords =
         Keyword.select do |k|
@@ -82,19 +84,32 @@ class IdeasController < ApplicationController
   # PATCH/PUT /ideas/1
   # PATCH/PUT /ideas/1.json
   def update
-    keyword_ids = params[:idea][:keyword_ids]      
-    keywords =
-      Keyword.select do |k|
-        keyword_ids.include? k.id.to_s
+    @idea = Idea.find(params[:id])
+    # get user to add comment, for now just one default user
+    user = User.first
+    comment_body = params[:comment_body]
+    unless comment_body.nil? or comment_body.empty?
+      comment = Comment.build_from(@idea, user.id, comment_body)
+      comment.save
+    end
+    if(params.has_key?(:idea))
+      #unless params[:idea][:keyword_ids].nil? or params[:idea][:keyword_ids].empty?    
+      keyword_ids = params[:idea][:keyword_ids]
+      keywords =
+        Keyword.select do |k|
+          keyword_ids.include? k.id.to_s
+        end
+      respond_to do |format|
+        if @idea.update(idea_params)
+          format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: 'edit' }
+          format.json { render json: @idea.errors, status: :unprocessable_entity }
+        end
       end
-    respond_to do |format|
-      if @idea.update(idea_params)
-        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
+    else
+      redirect_to @idea
     end
   end
 
@@ -116,6 +131,11 @@ class IdeasController < ApplicationController
 
   def set_keywords
     @words = Keyword.all
+  end
+
+  def set_comments
+    idea = Idea.find(params[:id])
+    @comments = Comment.find_comments_for_commentable( "Idea", idea.id )
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
