2e11d13fc449c9381d1b846bf5bbc6a582013dd8
Satu Kärkkäinen <iltasatu@gmail.com>
2014-02-25 13:41:56 +0100

    Unique keyword name validation

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,5 +1,6 @@
 class Keyword < ActiveRecord::Base
   validates_presence_of :name
+  validates :name, uniqueness: true
 
   has_many :idea_keywords, :dependent => :destroy
   has_many :ideas, through: :idea_keywords
